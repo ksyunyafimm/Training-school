86f52c40730b52f5614f19f5580beb73826c6728
ksyunyafimm <ksyunyafimm0498@gmail.com>
2022-12-22 19:38:30 +0300

    new project form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
+  form: FormGroup
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = new FormGroup({
+      firstName: new FormControl('',[Validators.minLength(2),Validators.required]),
+      lastName: new FormControl('',[Validators.minLength(2),Validators.required]),
+      email: new FormControl('',[Validators.email,Validators.required]),
+      adress: new FormGroup({
+        country: new FormControl('us'),
+        city: new FormControl('', Validators.required)
+      }),
+      phone: new FormControl('+'),
+      message: new FormControl('', [Validators.required,Validators.minLength(10),Validators.maxLength(500)]),
+      checkbox: new FormControl('',Validators.required)
+    })
   }
 
+  submit() {
+    if (this.form.valid) {
+      const formData = {...this.form.value}
+      console.log(formData)
+    }
+    this.form.reset()
+
+  }
+
+  setCapital() {
+    const cityMap = {
+        us: 'Washington',
+        ca: 'Ottawa',
+        ru: 'Moscow',
+        uz: 'Tashkent',
+        fr: 'Paris'
+    }
+    const cityKey = this.form.get('adress').get('country').value
+    const city = cityMap[cityKey]
+    console.log(city)
+    this.form.patchValue({
+      adress: {city}
+    })
+    }
 }
